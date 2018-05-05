361a923a658e6b71a19006b2dc9976997bc47ec0
Berry  Semexan <bsemexan@gmail.com>
2018-05-05 19:57:51 +0000

    Done.

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ function createPost(){
   var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
   var postTemplate = _.template(document.getElementById("post-template").innerHTML);
   var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
-
-  document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
 }
 
 function postComment(){
